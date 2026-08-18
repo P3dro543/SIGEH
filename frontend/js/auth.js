1dfc81429202017972921aaab2d689d10dafc4b8
Pedro Romero <99851531+P3dro543@users.noreply.github.com>
2026-08-17 20:20:19 -0600

    Update login API endpoint to production URL

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,6 +1,6 @@
 const auth = {
   login: async (username, password) => {
-    const res = await fetch('http://localhost:3000/api/auth/login', {
+    const res = await fetch('https://sigeh-dusky.vercel.app/api/auth/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
